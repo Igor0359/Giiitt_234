e27880e028633b39683a5811ede18d455d7435d1
Igor <igor91st@email.com>
2024-11-25 23:56:25 +0200

    fff1aaa

---
+++ 1222.js
@@ -0,0 +1,11 @@
+function greet(name) {
+    console.log("Привет, " + name + "!");
+}fdddd
+
+function VVEQQA(name) {
+    console.log("Добрый день, " + name + "!");
+}fdddd
+
+function LL888e(name) {
+    console.log("Добрый вечер, " + name + "!");
+}fddddfff
