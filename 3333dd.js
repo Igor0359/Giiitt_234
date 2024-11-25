8c300dc6935b6323d8edae3d154fef9443858bd7
Igor <igor91st@email.com>
2024-11-26 00:19:47 +0200

    rqq3rqq

---
+++ 3333dd.js
@@ -0,0 +1,17 @@
+const arrrrr1 = [32,4,6];
+console.log(arrrrr[1]);ffwdaaaasdd
+
+const arrrrr2 = [42,4,6];
+console.log(arrrrr[1]);ffwdaaaasdd
+
+const arrrrr3 = [52,4,6];
+console.log(arrrrr[1]);ffwdaaaasdd
+
+const arrrrr4 = [62,4,6];
+console.log(arrrrr[1]);ffwdaaaasdd
+
+const arrrrr5 = [72,4,6];
+console.log(arrrrr[1]);ffwdaaaasdd
+
+const arrrrr6 = [72,4,6];
+console.log(arrrrr[1]);ffwdaaaasdd
--- AAA1.js
+++ AAA1.js
@@ -7,3 +7,4 @@ greet("Мария");
 
 const arrrrr = [2,4,6];
 console.log(arrrrr[1]);
+console.log(arrrrr[2]);
